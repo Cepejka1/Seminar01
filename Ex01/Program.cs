c87757a5aa128fe157a1c87eff0f5d26b7e6d33a
Sergey <glad-nevers@ya.ru>
2022-12-04 20:01:54 +0700

    0.вывести квадрат числа

---
+++ Ex01/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0. Вывести квадрат числа.
+
+Console.WriteLine("start"); // начало
+
+int number = 11; // Определили целое число
+int result = number * number; // Вычесление квадрата числа
+Console.WriteLine(result); // Вывод результата 
+
+Console.WriteLine("end"); // окончание
