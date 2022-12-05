0c644c53b6c2197a672219a2749b690d8085f520
Sergey <glad-nevers@ya.ru>
2022-12-05 14:58:28 +0700

    Решил 3 задачу другим способом

--- Homework03/Program.cs
+++ Homework03/Program.cs
@@ -1,13 +1,24 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
 //Напримет: 4 -> да; -3 -> нет; 7 -> нет.
 
-int number = 7;
+// int number = 7;
 
-if(number % 2 == 0)
+// if(number % 2 == 0)
+// {
+    // Console.WriteLine("Четное число");
+// }
+// else
+// {
+    // Console.WriteLine("Нечетное число");
+// }
+
+Console.Write("Введите число");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
 {
-    Console.WriteLine("Четное число");
+    if ( i % 2 == 0)
+    {
+        Console.Write(i+" ");
+    }
 }
-else
-{
-    Console.WriteLine("Нечетное число");
-}
+Console.ReadKey();
